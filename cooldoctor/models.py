274089cc8148254e12a41f7d892106aa78ccd4a6
nikolai <ngbaranov@gmail.com>
2020-08-02 11:46:17 +0300

    Models final betta

--- cooldoctor/models.py
+++ cooldoctor/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Header(models.Model):
+    title = models.CharField(max_length=250, default='Cool', verbose_name='Название')
     avatar = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Аватарка')
     slogan = models.TextField(blank=True, verbose_name='Слоган')
     work_hours = models.CharField(max_length=250, verbose_name='Часы работы')
@@ -9,9 +11,88 @@ class Header(models.Model):
     fon = models.CharField(max_length=250, verbose_name='Телефон')
 
     def __str__(self):
-        return 'Классный доктор'
+        return self.title
 
     class Meta:
         verbose_name = 'Данные компании'
         verbose_name_plural = 'Данные компании'
+
+class TopMenu(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Контент')
+
+    def get_absolute_url(self):
+        return reverse('view_topmenu', kwargs={'pk':self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Информация о компании'
+        verbose_name_plural = 'Информация о компании'
+
+class Doctor(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    content = models.TextField(blank=True, verbose_name='Контент')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото доктора')
+    specialization = models.ForeignKey('Specialization', on_delete=models.PROTECT, verbose_name='Специализация')
+
+    def get_absolute_url(self):
+        return  reverse('view_doctor', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Доктор'
+        verbose_name_plural = 'Доктора'
+
+class Specialization(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='Специализация')
+
+    def get_absolute_url(self):
+        return reverse('specialization', kwargs={'specialization_id':self.pk})
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Специализация"
+        verbose_name_plural = 'Специализации'
+        ordering = ['title']
+
+
+class Action(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Акции')
+
+    class Meta:
+        verbose_name = 'Акция'
+        verbose_name_plural = 'Акции'
+
+class Discounts(models.Model):
+    title=models.CharField(max_length=150, verbose_name='Название')
+    content= models.TextField(verbose_name='Контент')
+
+    def get_absolute_url(self):
+        return reverse('discount', kwargs={'pk':self.pk})
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Скидки'
+
+class Article(models.Model):
+    title=models.CharField(max_length=150, verbose_name='Название')
+    content= models.TextField(verbose_name='Контент')
+
+    def get_absolute_url(self):
+        return reverse('article', kwargs={'pk':self.pk})
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+
+
+
+
 
--- cooldoctor/views.py
+++ cooldoctor/views.py
@@ -2,13 +2,30 @@ from django.shortcuts import render
 from .models import Header
 from django.views.generic import ListView, DetailView
 
+
+
+
+
 class HeaderInfo(ListView):
     model = Header
     template_name = 'cooldoctor/index.html'
     context_object_name = 'home'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Header.objects.get(pk = 1).title
+        context['work_hours'] = Header.objects.get(pk = 1).work_hours
+        context['address'] = Header.objects.get(pk = 1).address
+        context['avatar'] = Header.objects.get(pk = 1).avatar
+        context['slogan'] = Header.objects.get(pk = 1).slogan
+        context['fon'] = Header.objects.get(pk = 1).fon
 
-def index(request):
-    return render(request, 'cooldoctor/index.html')
+        return context
+
+
+
+
+# def index(request):
+#     return render(request, 'cooldoctor/index.html')
 
 
