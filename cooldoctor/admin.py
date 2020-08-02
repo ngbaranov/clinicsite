937977caec3293c6721e15b5951b087da9aeeeae
nikolai <ngbaranov@gmail.com>
2020-08-02 15:53:28 +0300

    admin betta

--- cooldoctor/admin.py
+++ cooldoctor/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
-# Register your models here.
-from cooldoctor.models import Header
+from cooldoctor.models import *
 
 admin.site.register(Header)
+admin.site.register(TopMenu)
+admin.site.register(Doctor)
+admin.site.register(Specialization)
+admin.site.register(Action)
+admin.site.register(Discounts)
+admin.site.register(Article)
